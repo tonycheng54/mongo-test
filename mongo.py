bf563f1b0c5ca56d4792d4a93fea8d8c96355ba9
Tony Cheng <1605008@iisigroup.com>
2024-01-07 12:33:40 +0800

    add group

--- mongo.py
+++ mongo.py
@@ -12,29 +12,48 @@ class Mongo:
         client = self.mongo_conn()
         collection = client[db][table]
 
-        collection.insert_many(json_data)
+        r = collection.insert_many(json_data)
+        return r
 
-    def mongo_find(self, db, table, query = {}, column = {'_id':0}):
+    def mongo_find(self, db, table, where = {}, column = {'_id':0}, print_flag = False):
         client = self.mongo_conn()
         collection = client[db][table]
         
         if ('_id' in column) == False:
             column['_id'] = 0
 
-        # print('query: %s' %type(query))
-        # print('column: %s'%type(column))
-        datas = collection.find(query,column)
+        if print_flag == True:
+            print('where: %s' %where)
+            print('column: %s'%column)
+        
+        datas = collection.find(where,column)
         r = list(datas)
         return r
-    def mongo_update(self, db, table, query = {}, update={}, upsert = False):
+    def mongo_update(self, db, table, where = {}, update={}, upsert = False):
         client = self.mongo_conn()
         collection = client[db][table]
         
-        result = collection.update_many(query, {"$set":update}, upsert=upsert)
+        result = collection.update_many(where, {"$set":update}, upsert=upsert)
         return result
     
-    def mongo_upsert(self,db, table, query = {}, update={}, upsert = True):
+    def mongo_upsert(self,db, table, where = {}, update={}, upsert = True):
         client = self.mongo_conn()
         collection = client[db][table]
-        res = collection.update_one(query, {"$set": update}, True)
+        res = collection.update_one(where, {"$set": update}, True)
         return res
+    
+    def mongo_group(self, db, table, group = {}, statistics = {}, where = {}):
+        client = self.mongo_conn()
+        collection = client[db][table]
+
+        if group == {} or statistics == {}:
+            return []
+        group = {"_id": group}
+        merge = {**group, **statistics}        
+        group = {"$group": merge}
+        match = {"$match":where}
+
+        # print("group: %s\nmatch: %s" %(group, match))
+
+        result = collection.aggregate([match, group])
+        return result
