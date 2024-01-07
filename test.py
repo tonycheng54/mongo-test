7629fa62f345c29256ca5250d3b427e5a57726b5
Tony Cheng <1605008@iisigroup.com>
2024-01-07 12:34:25 +0800

    add test code

--- test.py
+++ test.py
@@ -1,49 +1,11 @@
 from mongo import Mongo
 import datetime
-
-# class Mongo:
-#     def __init__(self) -> None:
-#         pass
-
-#     def mongo_conn(self):
-#         client = pymongo.MongoClient("mongodb://localhost:27017/")
-#         return client
-    
-#     def mongo_insert(self, db, table, json_data):
-#         client = self.mongo_conn()
-#         collection = client[db][table]
-
-#         collection.insert_many(json_data)
-
-#     def mongo_find(self, db, table, query = {}, column = {'_id':0}):
-#         client = self.mongo_conn()
-#         collection = client[db][table]
-        
-#         if ('_id' in column) == False:
-#             column['_id'] = 0
-
-#         # print('query: %s' %type(query))
-#         # print('column: %s'%type(column))
-#         datas = collection.find(query,column)
-#         r = list(datas)
-#         return r
-#     def mongo_update(self, db, table, query = {}, update={}, upsert = False):
-#         client = self.mongo_conn()
-#         collection = client[db][table]
-        
-#         result = collection.update_many(query, {"$set":update}, upsert=upsert)
-#         return result
-    
-#     def mongo_upsert(self,db, table, query = {}, update={}, upsert = True):
-#         client = self.mongo_conn()
-#         collection = client[db][table]
-#         res = collection.update_one(query, {"$set": update}, True)
-#         return res
-        
+import os        
 
 def find_test( mongo, title = "", db='db1', table='obs', query = {}, column = {}):
     datas = mongo.mongo_find(db, table, query, column)
-    print("\n%s" %title)
+    if title != "":
+        print("\n%s" %title)
     for d in datas:
         print(d)
 
@@ -54,7 +16,20 @@ def update_test(mongo:Mongo, title = "", db="db1", table = "obs", query = {}, up
         r = mongo.mongo_upsert(db, table, query, update)
     print("\n%s" %title)
     print(r)
-    pass
+    
+
+def insert_test(mongo:Mongo, title = "", db="db1", table = "obs", insert_data = []):
+    r = mongo.mongo_insert(db, table, insert_data)
+    print("\n%s" %title)
+    print(r)
+
+def convert_timestring_to_timeformat(mongo:Mongo,db="db1", talbe="obs"):
+    
+    rs = mongo.mongo_find(db, table, {"obstime": {"$not" : {"$type": "date"}}})
+
+    for r in rs:
+        d = datetime.datetime.strptime(r['obstime'], '%Y-%m-%dT%H:%M')
+        mongo.mongo_update(db, table, query={"stid": r['stid'], "obstime":r['obstime']}, update={"obstime": d})
 
 if __name__ == '__main__':
     mongo = Mongo()
@@ -63,33 +38,58 @@ if __name__ == '__main__':
     #     print(d)
     db = 'db1'
     table = 'obs'
-    #mongo.mongo_insert(db, table, [{	"stid": "466910",	"obstime": "2023-01-01T01:00",	"obs": {		"ws": 12.1,		"rain": 20,		"temp": 27.1	},	"qc": {		"ws": [			"method1",			"method2"		]	}}])
-    #find_test(mongo, db, table)
+    conn = mongo.mongo_conn()
+    conn[db][table].drop()
+    
 
+    timeformat = "%Y-%m-%d %H:%M"
+    d1 = datetime.datetime.strptime("2023-01-01 01:00", timeformat)
+    d2 = datetime.datetime.strptime("2023-01-01 02:00", timeformat)
+    d3 = datetime.datetime.strptime("2023-01-01 03:00", timeformat)
+    insert_data = [{"stid":"466900","obstime":d1, "obs":{"ws":12,"wd":240,"rain":37,"temp":30.2},"qc":{"rain":["method1","method2"],"ws":["method3"],"wd":["method1"]}},
+                   {"stid":"466910","obstime":d1, "obs":{"ws":12.1,"rain":20,"temp":27.1},"qc":{"ws":["method1","method2"]}},
+                   {"stid":"466900","obstime":d2,"obs":{"ws":17,"wd":230,"rain":11},"qc":{"rain":["method1","method3"]}},
+                   {"stid":"466900","obstime":d3,"obs":{"rain":2, "temp":11},"qc":{"temp":["method1","method2"]}}
+                   ]
+    insert_test(mongo, title="insert test data",insert_data=insert_data)
+
+    
+    
     find_test(mongo, "get all" )
     stid = '466900'
     find_test(mongo, title= "get station: %s" %stid, query= {"stid":stid})
     find_test(mongo, title="get all and select obs data", column={"obs":1})
     find_test(mongo, title="get rain > 10", query={'obs.rain':{"$gt":10}}, column={"obs":1,"stid":1, "obstime":1})
+    #os.system("pause")
+    update_test(mongo, title="update 2023-01-01 01:00 466900  temperature from 30.2 to 19.7", query={"stid":"466900", "obstime":d1}, update={"obs.temp":19.6})
+    find_test(mongo,query={"stid":"466900", "obstime":d1})
+    #os.system("pause")
+    update_test(mongo, title="update 2023-01-01 02:00 466900 add temperature 18.1", query={"stid":"466900", "obstime": d2}, update={"obs.temp":18.1})
+    find_test(mongo, query={"stid":"466900", "obstime": d2})
+    #os.system("pause")
 
-    update_test(mongo, title="update 2023/01/01 1:00 466900  temperature from 30.2 to 19.7", query={"stid":"466900", "obstime":"2023-01-01T01:00"}, update={"qc.temp":19.6})
-    
-    d = datetime.datetime.strptime("2023-01-01T01:00", "%Y-%m-%dT%H:%M")
     update_test(mongo, title="update none exist data and insert", 
-                query={"stid":"466880", "obstime": d}, 
-                update={"stid":"466880","obstime":d,"obs":{"ws":12.1,"rain":7.4,"temp":15.2},"qc":{"ws":["method1","method2"]}},
+                query={"stid":"466880", "obstime": d1}, 
+                update={"stid":"466880","obstime":d1,"obs":{"ws":12.1,"rain":7.4,"temp":15.2},"qc":{"ws":["method1","method2"]}},
                 upsert=True
                 )
+    find_test(mongo, query={"stid":"466880", "obstime": d1})
+    #os.system("pause")
     
-    update_test(mongo, title="update none exist data and insert", 
-                query={"stid":"466880", "obstime": d}, 
-                update={"stid":"466880","obstime":d,"obs":{"ws":12.1,"rain":27.4,"temp":15.2},"qc":{"ws":["method1","method2"]}}
-                
-                )
     
-    rs = mongo.mongo_find(db, table, {"obstime": {"$not" : {"$type": "date"}}})
+    find_test(mongo, title="get obstime > 2023-01-01 01:00", query={"obstime":{"$gt":datetime.datetime(2023, 1, 1, 1, 0, 0)}})
+    
+    table = "obsday"
+    statistic_start = datetime.datetime.strptime('2023-01-01 00:00', timeformat)
+    statistic_end =   datetime.datetime.strptime('2023-01-02 00:00', timeformat)
+    match = {"obstime":{"$gte":statistic_start, "$lt": statistic_end}}
 
-    for r in rs:
-        d = datetime.datetime.strptime(r['obstime'], '%Y-%m-%dT%H:%M')
-        mongo.mongo_update(db, table, query={"stid": r['stid'], "obstime":r['obstime']}, update={"obstime": d})
-
+    group = {"_id":{"stid":"$stid"}}
+    statistic = {"rain":{"$sum":"$obs.rain"}, "ws":{"$max":"obs.ws"}}
+    print("group: %s" %group)
+    print("statistic: %s" %statistic)
+    print("match:%s" %match)
+    #os.system("pause")
+    result = mongo.mongo_group(db, table, group = group, statistics = statistic, where = match)
+    for r in result:
+        print(r)
---
+++ test2.py
@@ -0,0 +1,83 @@
+from mongo import Mongo
+import datetime
+import os
+
+
+def compose_statistic(field, ope, output_field):
+    result = {}
+    for d in zip(field, ope, output_field):
+        result[d[2]] = {d[1]: "$%s"% d[0]}
+
+    return result
+
+
+def find_test( mongo, title = "", db='db1', table='obs', query = {}, column = {}):
+    datas = mongo.mongo_find(db, table, query, column)
+    if title != "":
+        print("\n%s" %title)
+    for d in datas:
+        print(d)
+
+def get_extreme_attribute(db, table):
+    start_time = datetime.datetime(2023,1,1,0,0)
+    end_time = datetime.datetime(2023,1,2,0,0)
+    match = {"obstime":{"$gte":start_time, "lt":end_time}}
+    group = {"stid":"$stid"}
+    
+
+
+if __name__ == '__main__':
+    mongo = Mongo()
+    db = 'db1'
+    table = 'obs'
+    
+    statistic_start = datetime.datetime(2023, 1, 1, 0, 0)
+    statistic_end =   datetime.datetime(2023, 1, 2, 0, 0)
+    find_test(mongo, title="print all data", db= db, table=table)
+    print("\n")
+    
+    field = ["obs.rain", "obs.ws", "obs.temp", "obs.temp"]
+    ope = ["$sum", "$max", "$max", "$min"]
+    output_field = ["rain", "ws", "max_temp", "min_temp"]
+    
+    match = {"obstime":{"$gte":statistic_start, "$lt": statistic_end}}
+    group = {"stid":"$stid"}
+    #statistic = {"rain":{"$sum":"$obs.rain"}, "ws":{"$max":"$obs.ws"}, "temp":{"$min":"$obs.temp"}}
+    statistic = compose_statistic(field=field, ope=ope, output_field=output_field)
+
+    # print("group: %s" %group)
+    # print("statistic: %s" %statistic)
+    # print("match:%s" %match)
+    
+    result = mongo.mongo_group(db, table, group = group, statistics = statistic, where = match)
+    obsday_datas = []
+    for r in result:
+        print(r)
+        stid = r["_id"]['stid']
+        print("station: %s" %stid)
+        print("rain: %s  ws: %s, min_temp: %s, max_temp: %s" %(r['rain'], r['ws'], r['min_temp'], r['max_temp']))
+        rain_occur_time = mongo.mongo_find(db, table, {"stid": stid, "rain": r['rain'], "obstime":{"$gte":statistic_start, "$lt": statistic_end}}, {"obstime":1})
+        wd_occur_time = mongo.mongo_find(db, table, {"stid": stid, 'obs.ws':r['ws'], "obstime":{"$gte":statistic_start, "$lt": statistic_end}}, {'obstime':1, 'obs.wd':1}, print_flag=True)
+        
+        print(wd_occur_time)
+        os.system("pause")
+        min_temp_occur_time = mongo.mongo_find(db, table, {"stid": stid, 'obs.temp':r['min_temp'], "obstime":{"$gte":statistic_start, "$lt": statistic_end}}, {'obstime':1,})
+        max_temp_occur_time = mongo.mongo_find(db, table, {"stid": stid, 'obs.temp':r['max_temp'], "obstime":{"$gte":statistic_start, "$lt": statistic_end}}, {'obstime':1,})
+        data = {
+            "stid":stid,
+            "obstime":datetime.datetime(statistic_start.year, statistic_start.month, statistic_start.day),
+            "obs":{
+                "ws": r['ws'],
+                "wd": wd_occur_time[0]['obs']['wd'] if 'wd' in  wd_occur_time[0]['obs'] else None,
+                "rain": r['rain'],
+                "max_temp": r['max_temp'],
+                "max_temp_occtime": min_temp_occur_time[0]
+            }
+        }
+        obsday_datas.append(data)
+
+    
+    table = "obsday"
+    mongo.mongo_insert(db, table, obsday_datas)
+
+    
