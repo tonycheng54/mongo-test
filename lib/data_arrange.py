a113d3127041651d0cd0602d2aa96b178d866268
Tony Cheng <1605008@iisigroup.com>
2024-03-01 13:48:11 +0800

    新增array資料插入的處理機制

--- lib/data_arrange.py
+++ lib/data_arrange.py
@@ -29,6 +29,9 @@ class MongoDataArrange:
         
         return self.__time_format(updatetime, timezone)
     
+    def __arrange_append_array_format(self, append_data, position):
+        return {'$each':[append_data], '$position':position}
+    
     def get_id(self, stno, obstime, timezone= 'Asia/Taipei'):
         return  stno + obstime.strftime('%Y%m%d%H%M%S')
 
@@ -36,42 +39,51 @@ class MongoDataArrange:
         dic = dict()
         if obstime is None or stno is None:
             raise "missing stno or obstime"
-        dic[Schema.id] = self.get_id(stno, obstime)
+        dic[Schema.id] = self.get_id(stno, obstime, timezone)
         dic[Schema.stationid] = stno
         dic[Schema.obstime] = self.__time_format(obstime, timezone)
         #dic[Schema.obstime] = datetime.datetime.strptime(obstime, timeformat)
         return dic
     
-    def pressure_arrange(self, dic:dict, pressure, updatetime = None, isappend=False):
-        if isappend == False:
-            dic[Schema.Pressure.key] = []
+    def pressure_arrange(self, dic:dict,  updatetime, pressure, qc_name_list:list=None, qc_result_list:list=None, isappend=False):
         data = dict()
         data[Schema.Pressure.updatetime] = self.__set_updatetime(updatetime)
         data[Schema.Pressure.pressure] = pressure
-        data[Schema.Pressure.qc_key] = dict()
-        data[Schema.Pressure.qc_key][Schema.Pressure.QC.qc_name] = 'cond'
-        data[Schema.Pressure.qc_key][Schema.Pressure.QC.result] = 'success'
+        if qc_name_list != None and qc_result_list != None:
+            data[Schema.Pressure.QC.key] = self.qc_arrange(qc_name_list, qc_result_list)
+
         if isappend == False:
+            dic[Schema.Pressure.key] = []
             dic[Schema.Pressure.key].append(data)
         else:
-            dic[Schema.Pressure.key] = data
+            #d = {'$each':[data], '$position':0}
+            dic[Schema.Pressure.key] = self.__arrange_append_array_format(data, 0)
         return dic
+
+        # data = dict()
+        # data[Schema.Pressure.updatetime] = self.__set_updatetime(updatetime)
+        # data[Schema.Pressure.pressure] = pressure
+        # #data[Schema.Pressure.qc_key] = self.qc_arrange( ['cond'], ['success'])
+        
+        # if isappend == False:
+        #     dic[Schema.Pressure.key].append(data)
+        # else:
+        #     dic[Schema.Pressure.key] = data
+        # return dic
     
-    def tx_arrange(self, dic: dict, tx, updatetime = None, isappend=False):
-        if isappend == False:
-            dic[Schema.Tx.key] = []
+    def tx_arrange(self, dic: dict, updatetime, tx, qc_name_list:list=None, qc_result_list:list=None,  isappend=False):
+
         data = dict()
         data[Schema.Tx.updatetime] = self.__set_updatetime(updatetime=updatetime)
         data[Schema.Tx.tx] = tx
         if isappend == False:
+            dic[Schema.Tx.key] = []
             dic[Schema.Tx.key].append(data)
         else:
-            dic[Schema.Tx.key] = data
+            dic[Schema.Tx.key] = self.__arrange_append_array_format(data, 0)
         return dic
-    
-    def wind_arragen(self, dic: dict, wd, ws, wg = None, updatetime = None, isappend= False):
-        if isappend == False:
-            dic[Schema.Wind.key] = []
+        
+    def wind_arrange(self, dic: dict, updatetime, wd, ws, wg = None, qc_name_list:list=None, qc_result_list:list=None, isappend=False):
         
         data = dict()
         data[Schema.Wind.updatetime] = self.__set_updatetime(updatetime)
@@ -80,24 +92,82 @@ class MongoDataArrange:
         if wg is not None:
             data[Schema.Wind.wg] = wg
         if isappend == False:
+            dic[Schema.Wind.key] = []
             dic[Schema.Wind.key].append(data)
         else:
-            dic[Schema.Wind.key] = data
-        
+            dic[Schema.Wind.key] = self.__arrange_append_array_format(data, 0)
         return dic
     
-    def rain_arrange(self, dic: dict, precp, rh = None, updatetime = None, isappend=False):
-        if isappend == False:
-            dic[Schema.Rain.key] = []
+    def rain_arrange(self, dic: dict, updatetime, precp, rh = None, qc_name_list:list=None, qc_result_list:list=None, isappend=False):        
         data = dict()
         data[Schema.Wind.updatetime] = self.__set_updatetime(updatetime)
         data[Schema.Rain.precp] = precp
         if rh is not None:
             data[Schema.Rain.rh] = rh
+
         if isappend == False:
+            dic[Schema.Rain.key] = []
             dic[Schema.Rain.key].append(data)
         else:
-            dic[Schema.Rain.key] = data
+            dic[Schema.Rain.key] = self.__arrange_append_array_format(data, 0)
         return dic
-        
 
+    def qc_arrange(self, qc_name_list:list, qc_result_list:list):
+        if len(qc_name_list) != len(qc_result_list):
+            raise "name size is diffrent than result"
+        data = []
+        for combine in zip(qc_name_list, qc_result_list):
+            dic = dict()
+            dic[Schema.AttributeBase.QC.name] = combine[0]
+            dic[Schema.AttributeBase.QC.result] = combine[1]
+            data.append(dic)
+        return data
+    
+    def qc_arrange1(self, dic, obs_key, qc_name_list, qc_result_list, isappend=False):
+        '''
+        產製QC格式
+        '''
+        if len(qc_name_list) != len(qc_result_list):
+            raise "name size is diffrent than result"
+        
+        data = []
+        for combine in zip(qc_name_list, qc_result_list):
+            d = dict()
+            d[Schema.AttributeBase.QC.key] = combine[0]
+            d[Schema.AttributeBase.QC.result] = combine[1]
+            data.append(d)
+
+        if isappend == False:
+            print(dic[obs_key])
+            dic[obs_key][0][Schema.AttributeBase.QC.key] = data
+        else:
+            key = "%s.0.%s"%(obs_key,Schema.AttributeBase.QC.key)
+            dic[key] = {"$each":data}
+
+        # data = []
+        # for combine in zip(qc_name_list, qc_result_list):
+        #     dic = dict()
+        #     dic[Schema.QC.name] = combine[0]
+        #     dic[Schema.QC.result] = combine[1]
+        #     data.append(dic)
+        # print(data)
+        # if isappend == False:
+        #     dic[obs_key][0][Schema.__AttributeBase.qc_key] = data
+        # else:
+        #     print(dic)
+        #     key = "%s.0.%s"%(obs_key,Schema.Rain.qc_key)
+        #     dic[key] = {"$each":data}
+        return dic
+        #dic[Schema.Pressure.key][0][Schema.Pressure.qc_key] = arrange.qc_arrange(['即時品管-氣壓'], ['pass'])
+
+    def qc_append_arrange(self, obs_key, qc_name_list, qc_result_list):
+        '''
+        嘗試與qc_arrange合併
+        '''
+        dic = dict()
+        key = "%s.0.%s"%(obs_key,Schema.AttributeBase.QC.key)
+        if len(qc_name_list) != len(qc_result_list):
+            raise "name size is diffrent than result"
+        qcdata = self.qc_arrange(qc_name_list= qc_name_list, qc_result_list=qc_result_list)
+        dic[key] = {"$each":qcdata}
+        return dic
